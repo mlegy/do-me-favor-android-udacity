4445b1e1087f1f0e196667b397db5b728886c3d0
Ahmad El-Melegy <ahmad.melegy@gmail.com>
2017-04-04 23:15:57 +0200

    Add models

---
+++ app/src/main/java/melegy/com/domeafavour/models/resources/Favor.java
@@ -0,0 +1,45 @@
+
+package melegy.com.domeafavour.models.resources;
+
+import android.os.Parcelable;
+
+import com.google.auto.value.AutoValue;
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by ahmad on 4/3/17.
+ */
+
+@AutoValue
+public abstract class Favor implements Parcelable {
+
+    @SerializedName("title")
+    abstract String title();
+
+    @SerializedName("description")
+    abstract String description();
+
+    @SerializedName("owner")
+    abstract User owner();
+
+    @SerializedName("benefactor")
+    abstract User benefactor();
+
+    @SerializedName("location")
+    abstract Location location();
+
+    @SerializedName("is_done")
+    abstract Boolean done();
+
+    public static TypeAdapter<Favor> typeAdapter(Gson gson) {
+        return new $AutoValue_Favor.GsonTypeAdapter(gson);
+    }
+
+    public static Favor create(String title, String description, User owner, User benefactor,
+            Location location, Boolean done) {
+        return new AutoValue_Favor(title, description, owner, benefactor, location, done);
+    }
+
+}
