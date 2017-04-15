a415b12d8098753f668360a5851da18076fdb97d
Ahmad El-Melegy <ahmad.melegy@gmail.com>
2017-04-15 16:39:01 +0200

    Add basic API skelton

---
+++ app/src/main/java/melegy/com/domeafavour/models/responses/LoginResponse.java
@@ -0,0 +1,33 @@
+
+package melegy.com.domeafavour.models.responses;
+
+import android.support.annotation.Nullable;
+
+import com.google.auto.value.AutoValue;
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.SerializedName;
+
+import melegy.com.domeafavour.models.resources.Error;
+import melegy.com.domeafavour.models.resources.User;
+
+/**
+ * Created by ahmad on 4/11/17.
+ */
+
+@AutoValue
+public abstract class LoginResponse {
+
+    @Nullable
+    @SerializedName("user")
+    abstract User user();
+
+    @Nullable
+    @SerializedName("error")
+    abstract Error error();
+
+    public static TypeAdapter<LoginResponse> typeAdapter(Gson gson) {
+        return new AutoValue_LoginResponse.GsonTypeAdapter(gson);
+    }
+
+}
