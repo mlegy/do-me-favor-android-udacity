036ee3f3a6c2db58c0c88c78f87869fa879fa9c3
Ahmad El-Melegy <ahmad.melegy@gmail.com>
2017-05-12 11:47:35 +0200

    Add async task for add favor request

--- app/src/main/java/melegy/com/domeafavour/features/favors/addFavor/AddFavorFragment.java
+++ app/src/main/java/melegy/com/domeafavour/features/favors/addFavor/AddFavorFragment.java
@@ -3,7 +3,9 @@ package melegy.com.domeafavour.features.favors.addFavor;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -14,11 +16,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 import com.tbruyelle.rxpermissions.RxPermissions;
+
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import javax.inject.Inject;
 
@@ -28,9 +39,6 @@ import melegy.com.domeafavour.App;
 import melegy.com.domeafavour.R;
 import melegy.com.domeafavour.features.favors.favorsFeed.FeedActivity;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class AddFavorFragment extends Fragment implements
         GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
@@ -81,10 +89,11 @@ public class AddFavorFragment extends Fragment implements
                         if (lastLocation != null) {
                             String title = editTextTitle.getText().toString();
                             String description = editTextDescription.getText().toString();
-                            addFavorVM.addFavor(title, description, lastLocation)
-                                    .subscribe(x -> FeedActivity.start(this.getActivity()),
-                                            throwable -> showMessage(view,
-                                                    R.string.something_wrong_error_message));
+
+                            // making async task, just to meet udacity requirements.
+                            new SendPostRequest(getActivity()).execute(title, description,
+                                    "590cf6e0f8970d00119c2c5b", lastLocation.getLongitude() + "",
+                                    lastLocation.getLongitude() + "");
                         }
                     } else
                         showMessage(view, R.string.no_permission_error_message);
@@ -132,4 +141,62 @@ public class AddFavorFragment extends Fragment implements
         }
     }
 
+    public static class SendPostRequest extends AsyncTask<String, Void, Integer> {
+        Context context;
+
+        private SendPostRequest(Context context) {
+            this.context = context.getApplicationContext();
+        }
+
+        protected Integer doInBackground(String... params) {
+            try {
+                URL url = new URL(
+                        "http://192.168.78.9:3000/favors?access_token=xE33zWa5TKCRNbGb6X8bvrv9erB5xh95k3a9fcAP");
+
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("title", params[0]);
+                postDataParams.put("description", params[1]);
+                postDataParams.put("owner", params[2]);
+
+                JSONObject location = new JSONObject();
+                location.put("long", Double.valueOf(params[3]));
+                location.put("lat", Double.valueOf(params[4]));
+
+                postDataParams.put("location", location);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(postDataParams.toString());
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                return conn.getResponseCode();
+
+            } catch (Exception e) {
+                return 500;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            if (result == 200) {
+                FeedActivity.start(context);
+            } else {
+                Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
